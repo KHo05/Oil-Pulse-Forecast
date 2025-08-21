8ad8e4b93df143ff71867b8e62cbb630655ebdd8
KHo05 <76831093+KHo05@users.noreply.github.com>
2025-08-21 14:18:39 +0800

    Update symbolssearch.py

--- scripts/symbolssearch.py
+++ scripts/symbolssearch.py
@@ -1,6 +1,7 @@
 import requests
 
-# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+# Replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+# Below is a free but limited API key for demo purposes
 url = 'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=oil&apikey=S8T54DZ7NA1DLK7I'
 r = requests.get(url)
 data = r.json()
